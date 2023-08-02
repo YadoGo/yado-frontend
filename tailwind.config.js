4ac1f88f312be8ea0d08f8ef65ef6753818b2051
Ernest Magriñá <ernest8081@gmail.com>
2023-08-02 07:08:19 +0200

    style: :lipstick: configure a custom daisyUI theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          "primary": "#ea4b37",
+          "secondary": "#fc8a72",
+        },
+      },
+      {
+        dark: {
+          "primary": "#ea4b37",
+          "secondary": "#fc8a72",
+        },
+      },
+    ],
+  },
 };
