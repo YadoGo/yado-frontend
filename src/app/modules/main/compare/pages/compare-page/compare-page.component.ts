c75f0d793e6b45f1fb67e57a8878f7f7a2f2d6c2
GitJanPlata <96839905+GitJanPlata@users.noreply.github.com>
2023-08-11 18:19:00 +0200

    feat: :package: base compare component

--- src/app/modules/main/compare/pages/compare-page/compare-page.component.ts
+++ src/app/modules/main/compare/pages/compare-page/compare-page.component.ts
@@ -3,8 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-compare-page',
   templateUrl: './compare-page.component.html',
-  styleUrls: ['./compare-page.component.css']
+  styleUrls: ['./compare-page.component.css'],
 })
 export class ComparePageComponent {
+  hotel1Attributes = [
+    { text: 'Lavado de ropa gratis', isBetter: this.randomBetter() },
+    {
+      text: 'Fruta a disposición en la habitación',
+      isBetter: this.randomBetter(),
+    },
+    { text: 'Sitio bien comunicado', isBetter: this.randomBetter() },
+    { text: 'Admiten mascotas', isBetter: this.randomBetter() },
+    { text: 'Piscina', isBetter: this.randomBetter() },
+    { text: 'Sala de juegos', isBetter: this.randomBetter() },
+    { text: 'Restaurante interno', isBetter: this.randomBetter() },
+    { text: '2 baños', isBetter: this.randomBetter() },
+    { text: 'Admiten perros', isBetter: this.randomBetter() },
+    { text: 'Prohibido fumar', isBetter: this.randomBetter() },
+    { text: 'Desayuno incluido', isBetter: this.randomBetter() },
+    { text: 'Ducha con hidromasaje', isBetter: this.randomBetter() },
+    { text: 'Wi-Fi gratuito', isBetter: this.randomBetter() },
+    { text: 'Gimnasio', isBetter: this.randomBetter() },
+  ];
 
+  hotel2Attributes = [
+    { text: 'Lavado de ropa gratis', isBetter: this.randomBetter() },
+    {
+      text: 'Fruta a disposición en la habitación',
+      isBetter: this.randomBetter(),
+    },
+    { text: 'Sitio bien comunicado', isBetter: this.randomBetter() },
+    { text: 'Admiten mascotas', isBetter: this.randomBetter() },
+    { text: 'Piscina', isBetter: this.randomBetter() },
+    { text: 'Sala de juegos', isBetter: this.randomBetter() },
+    { text: 'Restaurante interno', isBetter: this.randomBetter() },
+    { text: '2 baños', isBetter: this.randomBetter() },
+    { text: 'Admiten perros', isBetter: this.randomBetter() },
+    { text: 'Prohibido fumar', isBetter: this.randomBetter() },
+    { text: 'Desayuno incluido', isBetter: this.randomBetter() },
+    { text: 'Ducha con hidromasaje', isBetter: this.randomBetter() },
+    { text: 'Wi-Fi gratuito', isBetter: this.randomBetter() },
+    { text: 'Gimnasio', isBetter: this.randomBetter() },
+  ];
+
+  randomBetter(): boolean {
+    return Math.random() > 0.5;
+  }
 }
