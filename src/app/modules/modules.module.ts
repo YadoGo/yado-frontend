eec41513027fb2209ba7fcb0283d6d35d0b87554
David Tomas <david.tomas@grupocastilla.es>
2023-08-11 17:38:04 +0200

    feat: :package: add view TopHotelsPage

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -3,17 +3,28 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '@shared/shared.module';
 
-import { AuthLayoutComponent, LoginPageComponent, RegisterPageComponent } from './auth';
-import { HeroComponent, CtaMapComponent, HomePageComponent, PopularDestinationComponent } from './main/home';
+import {
+  AuthLayoutComponent,
+  LoginPageComponent,
+  RegisterPageComponent,
+} from './auth';
+import {
+  HeroComponent,
+  CtaMapComponent,
+  HomePageComponent,
+  PopularDestinationComponent,
+} from './main/home';
 import { MainLayoutComponent } from './main/layouts';
 import { AboutPageComponent } from './main/about';
 import { ComparePageComponent } from './main/compare';
 import { TopHotelsPageComponent } from './main/top-hotels';
-import { FiltersComponent, ListComponent, ListHotelsPageComponent, MapHotelsComponent } from './main/list-hotels';
-
-
-
-
+import {
+  FiltersComponent,
+  ListComponent,
+  ListHotelsPageComponent,
+  MapHotelsComponent,
+} from './main/list-hotels';
+import { HotelDetailsPopupComponent } from './main/top-hotels/components/hotel-details-popup/hotel-details-popup.component';
 
 @NgModule({
   declarations: [
@@ -31,12 +42,10 @@ import { FiltersComponent, ListComponent, ListHotelsPageComponent, MapHotelsComp
     ListHotelsPageComponent,
     FiltersComponent,
     ListComponent,
-    MapHotelsComponent
+    MapHotelsComponent,
+    HotelDetailsPopupComponent,
   ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    SharedModule
-  ]
+  imports: [CommonModule, RouterModule, SharedModule],
+  exports: [HotelDetailsPopupComponent],
 })
-export class ModulesModule { }
+export class ModulesModule {}
