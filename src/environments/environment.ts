df0dc71b5b82185a5eb49c0800f7306bf4f53a2d
Ernest Magriñá <ernest8081@gmail.com>
2023-09-19 10:59:00 +0200

    chore: :see_no_evil: add environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://localhost:7000',
+  championsApiUrl:
+    'https://api-football-v1.p.rapidapi.com/v3/fixtures?league=2&season=2023',
+  championsApiKey: 'bc12c9bff2msh3cd510d981abf66p12b945jsn9e1debc3d13b',
+};
